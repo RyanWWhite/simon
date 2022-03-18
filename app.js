2f52c95e876244dfa2318f4c7b0562dbd4e92a23
RyanWWhite <ryanwwhite2@gmail.com>
2022-03-18 15:07:10 -0230

    linked images

---
+++ app.js
@@ -0,0 +1,51 @@
+const minecraft = document.getElementById("minecraft");
+const oof = new Audio(
+  "https://www.myinstants.com/media/sounds/steve-old-hurt-sound_XKZxUk4.mp3"
+);
+
+minecraft.addEventListener("click", (e) => {
+  audio.play();
+});
+
+const nut = document.getElementById("nut");
+const audio = new Audio(
+  "https://www.myinstants.com/media/sounds/deez-nuts-sound-effect-free-download-hd-.mp3"
+);
+
+minecraft.addEventListener("click", (e) => {
+  audio.play();
+});
+
+const roblox = document.getElementById("roblox");
+const oofblox = new Audio(
+  "https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3"
+);
+
+minecraft.addEventListener("click", (e) => {
+  audio.play();
+});
+
+const windows = document.getElementById("windows");
+const shutdown = new Audio("https://www.myinstants.com/media/sounds/erro.mp3");
+
+minecraft.addEventListener("click", (e) => {
+  audio.play();
+});
+
+const fbi = document.getElementById("fbi");
+const openup = new Audio(
+  "https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3"
+);
+
+minecraft.addEventListener("click", (e) => {
+  audio.play();
+});
+
+const damage = document.getElementById("damage");
+const emotional = new Audio(
+  "https://www.myinstants.com/media/sounds/emotional-damage-meme.mp3"
+);
+
+minecraft.addEventListener("click", (e) => {
+  audio.play();
+});
