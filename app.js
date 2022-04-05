44a63cf45b300c89fcf9b0b5ff82c6964262f69d
RyanWWhite <ryanwwhite2@gmail.com>
2022-04-05 12:52:27 -0230

    changed buttons

--- app.js
+++ app.js
@@ -1,80 +1,65 @@
-const minecraft = document.getElementById("minecraft");
-const minercaftAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/steve-old-hurt-sound_XKZxUk4.mp3"
-);
+function addSound(name, audioSrc, imageSrc) {
+  const container = document.createElement("div");
+  container.classList.add("meme");
 
-minecraft.addEventListener("click", (e) => {
-  minercaftAudio.play();
-});
+  const button = document.createElement("button");
+  button.classList.add("btn");
+  button.id = name;
 
-const nut = document.getElementById("nut");
-const nutAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/deez-nuts-sound-effect-free-download-hd-.mp3"
-);
+  container.appendChild(button);
 
-nut.addEventListener("click", (e) => {
-  nutAudio.play();
-});
+  const img = document.createElement("img");
+  img.src = imageSrc;
 
-const roblox = document.getElementById("roblox");
-const robloxAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3"
-);
+  button.appendChild(img);
 
-roblox.addEventListener("click", (e) => {
-  robloxAudio.play();
-});
+  const buttonAudio = new Audio(
+    `https://www.myinstants.com/media/sounds/${audioSrc}.mp3`
+  );
 
-const windows = document.getElementById("windows");
-const windowsAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/erro.mp3"
-);
+  button.addEventListener("click", (e) => {
+    buttonAudio.play();
+  });
 
-windows.addEventListener("click", (e) => {
-  windowsAudio.play();
-});
+  document.getElementById("buttons").appendChild(container);
+}
 
-const fbi = document.getElementById("fbi");
-const fbiAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3"
-);
+const sounds = [
+  {
+    name: "minecraft",
+    audioSrc: "steve-old-hurt-sound_XKZxUk4",
+    imageSrc: "steve.png",
+  },
 
-fbi.addEventListener("click", (e) => {
-  fbiAudio.play();
-});
+  {
+    name: "nut",
+    audioSrc: "deez-nuts-sound-effect-free-download-hd-",
+    imageSrc: "nut.png",
+  },
 
-const damage = document.getElementById("damage");
-const damageAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/emotional-damage-meme.mp3"
-);
+  { name: "roblox", audioSrc: "roblox-death-sound_1", imageSrc: "roblox.png" },
 
-damage.addEventListener("click", (e) => {
-  damageAudio.play();
-});
+  { name: "windows", audioSrc: "erro", imageSrc: "windows.jpg" },
 
-const chewbacca = document.getElementById("chewbacca");
-const chewbaccaAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/chewbacca.swf.mp3"
-);
+  { name: "fbi", audioSrc: "fbi-open-up-sfx", imageSrc: "fbi.png" },
 
-chewbacca.addEventListener("click", (e) => {
-  chewbaccaAudio.play();
-});
+  {
+    name: "damage",
+    audioSrc: "emotional-damage-meme",
+    imageSrc: "damage.jpg",
+  },
 
-const amongus = document.getElementById("amongus");
-const amongusAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/among.mp3"
-);
+  {
+    name: "chewbacca",
+    audioSrc: "chewbacca.swf",
+    imageSrc: "chewbacca.jpg",
+  },
 
-damage.addEventListener("click", (e) => {
-  damageAudio.play();
-});
+  { name: "amongus", audioSrc: "among", imageSrc: "amongus.jpg" },
 
-const dababy = document.getElementById("dababy");
-const dababyAudio = new Audio(
-  "https://www.myinstants.com/media/sounds/dababy-suge-lyrics-1.mp3"
-);
+  { name: "dababy", audioSrc: "dababy-suge-lyrics-1", imageSrc: "dababy.jpg" },
+];
 
-damage.addEventListener("click", (e) => {
-  damageAudio.play();
-});
+for (let i = 0; i < sounds.length; i++) {
+  addSound(sounds[i].name, sounds[i].audioSrc, sounds[i].imageSrc);
+}
